0ba38507df2ad98fd8f89bd16444e6cf2387be1b
jwaltrip <jake.waltrip@gmail.com>
2018-09-27 11:20:15 -0500

    initial static component built

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Reddit Viewer</h1>
+        <div className="active-subreddit-name">/r/all</div>
+
+        <div className="post-container">
+          <div className="post">
+            <div className="post-upvotes">28.2k</div>
+            <div className="post-info">
+              <div className="post-title">
+                Dunkin- the dog that flies over everything no matter how tall they are.
+              </div>
+              <div className="post-footer">
+                <div className="post-subreddit">/r/aww</div>
+                <div className="post-user">u/JoseTwitterFan</div>
+                <div className="post-comments">282 comments</div>
+                <div className="post-time">posted 2 hours ago</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
